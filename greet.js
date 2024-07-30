6f78341ad2edcf366bb1f0338e9f090749debdfa
Lesego Modise <lesegonhlanhla18@gmail.com>
2024-07-30 15:35:02 +0200

    fix a typo

--- greet.js
+++ greet.js
@@ -1,6 +1,4 @@
-// export function greet(name) { //Just like any function, I named my funtion 'greet' followed by a 'name' parameter.
-//     return ('Hello, ' + name); //This is the result once the code passes.
-//   }
+
 
 
 export function greet(name) {
